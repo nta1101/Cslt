b599c366360a849b00191db7924ef7573db447e3
NTA <nta@NTAs-MacBook-Pro.local>
2021-04-18 00:54:55 +0700

    bat dau du an

---
+++ B2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("nhap n: ");
+    scanf("%d", &n);
+    int s = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        s += i * (i + 1);
+    }
+    printf("%d", &s);
+    return 0;
+}
---
+++ B3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    //int n;
+    //float m;
+    int A[10];
+    //printf("so phan tu n: ");
+    //scanf("%d", n);
+
+    //printf("nhap vao mot so thuc: ");
+    //scanf("%f", &m);
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("A[] = ");
+        scanf(A[i]);
+    }
+    return 0;
+}
